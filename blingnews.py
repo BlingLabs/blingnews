b792b16e601b79874231ef3ff8d244c08e3f3f49
= <=>
2013-09-08 03:26:41 -0400

    working on get user articles now

--- blingnews.py
+++ blingnews.py
@@ -3,7 +3,7 @@ import os
 
 import prediction
 import rss_collector
-from database.db import UserHandler
+from database import db
 
 from google.appengine.ext.webapp import template
 
@@ -18,8 +18,9 @@ class MainPage(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
   ('/', MainPage),
-  ('/api/user', UserHandler), #create /api/user
-  ('/api/user/(\d)+/(.*)', UserHandler), #eg. get /api/user/123/name
+  ('/api/user', db.UserHandler), #create /api/user
+  ('/api/user/(\d)+/(.*)', db.UserHandler), #eg. get /api/user/123/name
+  ('/api/article', db.ArticleHandler), #create /api/article
   ('/prediction', prediction.Prediction),
   (prediction.decorator.callback_path, prediction.decorator.callback_handler()) #Oauth dance
 ], debug=True)
--- database/db.py
+++ database/db.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import re
+import unicodedata
 import webapp2
 from google.appengine.api import rdbms
 
@@ -7,32 +9,82 @@ from google.appengine.api import rdbms
 INSTANCE_NAME = 'blingpenn:blingsql'
 DATABASE_NAME = 'blingnews'
 
-class UserHandler (webapp2.RequestHandler):
+
+class ArticleHandler(webapp2.RequestHandler):
   def get(self, user_id, attr):
-    """ Get the specified attribute. """
-    print user_id
-    print attr
+    pass
+
+  def post(self):
+    """ Put article in database.
+      Args:
+        title: string
+        link: string
+        body: string
+        author: string
+        source: string
+        date: string
+    """
+    title = self.request.get('title')
+    link = self.request.get('link')
+    body = self.request.get('body')
+    author = self.request.get('author')
+    source = self.request.get('source')
+    date = self.request.get('date')
 
     conn = rdbms.connect(instance=INSTANCE_NAME, database=DATABASE_NAME)
     cursor = conn.cursor()
 
-    SQL_GET_ATTR = 'SELECT %s FROM users WHERE id = %s'
-    cursor.execute(SQL_GET_ATTR, (attr, str(user_id)))
+    # make sure source is in database
+    SQL_INSERT_SOURCE = 'INSERT IGNORE INTO sources (name) VALUES (%s)'
+    cursor.execute(SQL_INSERT_SOURCE, (source))
 
-    #if cursor.rowcount == -1:
-    #  logging.error('Found nothing in database')
-    #  self.error(400)
-    #  return
+    SQL_GET_SOURCE_ID = 'SELECT id FROM sources where name like %s'
+    cursor.execute(SQL_GET_SOURCE_ID, (source))
+    if cursor.rowcount == -1:
+      #Source doesn't exist in database still.
+      logging.error('db insert error')
+      self.error(500)
+      return
 
-    # Fetch twice because first row is column name
-    
-    print cursor.rowcount
+    source_id = cursor.fetchone()[0]
 
-    result = cursor.fetchone()
+    # insert article
+    SQL_INSERT_ARTICLE = 'INSERT INTO articles (title, link, body, author, source_id, date) VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(SQL_INSERT_ARTICLE, (title, link, body, author, str(source_id), date))
 
-    json_resp = json.dumps({attr: str(result[1])})
+    conn.commit()
+    conn.close()
+
+
+class UserHandler(webapp2.RequestHandler):
+  def get(self, user_id, attr):
+    """ Get the specified attribute. """
+
+    if attr in ('fb_token', 'name'):
+      result = self.get_simple_attr(user_id, attr)
+    elif attr == 'articles':
+      result = self.get_user_articles(user_id)
+
+    if result is None:
+      logging.error('no result from db')
+      self.error(400)
+
+    json_resp = json.dumps({attr: str(result)})
     self.response.out.write(json_resp)
 
+  def get_user_articles(self, user_id)
+    pass
+
+  def get_simple_attr(self, user_id, attr)
+    conn = rdbms.connect(instance=INSTANCE_NAME, database=DATABASE_NAME)
+    cursor = conn.cursor()
+
+    SQL_GET_ATTR = u'SELECT %s FROM users WHERE id = %s'
+    cursor.execute(SQL_GET_ATTR % (attr, user_id))
+
+    if cursor.rowcount == -1:
+      return None
+    result = cursor.fetchone()[0]
 
   def post(self):
     """ Create user.
@@ -46,7 +98,7 @@ class UserHandler (webapp2.RequestHandler):
 
     conn = rdbms.connect(instance=INSTANCE_NAME, database=DATABASE_NAME)
     cursor = conn.cursor()
-    SQL_CREATE_USER = 'INSERT INTO users (name, fb_token) VALUES (%s, %s)'
+    SQL_CREATE_USER = r'INSERT INTO users (name, fb_token) VALUES (%s, %s)'
     cursor.execute(SQL_CREATE_USER, (name, fb_token))
 
     conn.commit()
