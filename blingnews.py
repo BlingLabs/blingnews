238def9ce99b27b16b8c931cdd68c9e520e68e0c
John Liu <johnyuhanliu@gmail.com>
2013-09-08 00:05:21 -0400

    Makes python app deploy ready.

--- blingnews.py
+++ blingnews.py
@@ -1,13 +1,17 @@
 import webapp2
+import os
 
 import prediction
 import rss_collector
 
+from google.appengine.ext.webapp import template
+
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'dist/index.html')
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.write('<h1>hello</h1><h2>there</h2>')
+    self.response.write(template.render(path, None))
 
     # self.response.headers['Content-Type'] = 'text/plain'
     # self.response.write(rss_collector.get_rss_data())
